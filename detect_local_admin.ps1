cd84113449ce2a5f0f41daf4ca1239df3c11d6da
fareschelloug <fares.chelloug@allopneus.com>
2023-02-18 07:23:32 +0100

    Update detect_local_admin.ps1

--- detect_local_admin.ps1
+++ detect_local_admin.ps1
@@ -1,8 +1,15 @@
-$admin = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-if ($admin.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Output "L'utilisateur en cours est un administrateur."
-    Exit 1
-} else {
-    Write-Output "L'utilisateur en cours n'est pas un administrateur."
-    Exit 0
-}
+# Get who I am
+$Me = whoami.exe
+
+ # Get members of administrators group
+$Admins = Get-LocalGroupMember -Name Administrateurs | 
+       Select-Object -ExpandProperty name
+
+# Check to see if this user is an administrator and act accordingly
+ if ($Admins -Contains $Me) {
+      "$Me is a local administrator"}
+     exit 1 
+    else {
+     "$Me is NOT a local administrator"}
+     exit 0
+     
